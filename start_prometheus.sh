575339b68b2c3030b2571007b52c659a41219002
Andrew-Miliyan <117437526+Andrew-Miliyan@users.noreply.github.com>
2023-12-17 09:15:15 +0200

    start_prometheus.sh

--- start_prometheus.sh
+++ start_prometheus.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
+sudo systemctl daemon-reload
+sudo systemctl start node_exporter.service
+sudo systemctl restart prometheus.service
 sudo systemctl enable prometheus
 sudo systemctl start prometheus
 sudo systemctl status prometheus
